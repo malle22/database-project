8ed9ffb3546039365c3ec2de5cc53289c35921f8
malle22 <96298237+malle22@users.noreply.github.com>
2023-12-19 15:44:50 +0100

    completed 2 functions for doctor

--- Medical_Center/src/MedicalServices.java
+++ Medical_Center/src/MedicalServices.java
@@ -63,11 +63,102 @@ public class MedicalServices {
                 System.out.println(", Record date: " + recordDate);
                 System.out.println("--------------------------------");
             }
-        } catch (SQLException e ){
+        } catch (SQLException e) {
             e.printStackTrace();
             throw new Exception("Error retrieving medical records.", e);
         }
     }
+
+    void changeAvailability(String e_num, String day, Time time) {
+        try {
+            // Establish a connection to the database
+            Connection con = getDatabaseConnection();
+
+            // Call the stored function
+            String query = "{ ? = call fn_change_availability(?, ?, ?) }";
+            CallableStatement callableStatement = con.prepareCall(query);
+
+            // Set the function parameters
+            callableStatement.registerOutParameter(1, java.sql.Types.BOOLEAN);
+            callableStatement.setString(2, e_num);
+            callableStatement.setString(3, day);
+            callableStatement.setTime(4, time);
+
+            // Execute the function
+            callableStatement.execute();
+
+            // Get the result
+            Boolean result = callableStatement.getBoolean(1);
+
+            if (result) {
+                System.out.println("Availability change successful");
+            } else {
+                System.out.println("Availability change failed. Appointment already booked.");
+            }
+
+            // Close resources
+            callableStatement.close();
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void listAllUpcomingAppointments(){
+        String query = "SELECT * FROM fn_list_all_appointments()";
+
+        try (Connection con = getDatabaseConnection();
+             PreparedStatement pstmt = con.prepareStatement(query)) {
+
+            // Execute the query
+            ResultSet rs = pstmt.executeQuery();
+
+            // Get result for each appointment
+            while (rs.next()) {
+                String app_day = rs.getString("app_day");
+                Time app_time = rs.getTime("app_time");
+                String patient = rs.getString("patient_full_name");
+                String doctor = rs.getString("doctor_full_name");
+
+                System.out.print("Day: " + app_day);
+                System.out.print(", Time: " + app_time);
+                System.out.print(", Patient: " + patient);
+                System.out.println(", Doctor: " + doctor);
+                System.out.println("--------------------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    void listAllUpcomingAppointmentsOfDoctor(String eNum){
+        String query = "SELECT * FROM fn_list_all_appointments_for_doctor(?)";
+
+        try (Connection con = getDatabaseConnection();
+             PreparedStatement pstmt = con.prepareStatement(query)) {
+            // Set input parameters
+            pstmt.setString(1, eNum);
+
+            // Execute the query
+            ResultSet rs = pstmt.executeQuery();
+            // Get result for each medical record
+            while (rs.next()) {
+                String day = rs.getString("app_day");
+                Time time = rs.getTime("app_time");
+                String patient = rs.getString("patient_full_name");
+
+                System.out.print("Day: " + day);
+                System.out.print(", Time: " + time);
+                System.out.println(", Patient: " + patient);
+                System.out.println("--------------------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void printAllPatients() throws Exception {
     Connection con = getDatabaseConnection();
